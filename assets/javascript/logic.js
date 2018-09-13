7af67922d23fc7444d18172bb46ad7b9dcbed4f9
Abz <s.martinez92@live.com>
2018-09-13 16:49:02 -0400

    finale

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,58 +1,118 @@
-var listOfGifs = ['dragon', 'giant squid', 'wolf', 'panther', 'unicorn'];
+$(document).ready(function () {
 
-function generatePics() {
-	$('.gifDiv').empty();
-
-	var gifName = $(this).attr('data-name');
-	var apiLink = "https://api.giphy.com/v1/gifs/search?api_key=snkpgzzy24XfiY7U9yoYkU93dX0FMQJF&q=" + gifName + "&limit=10&offset=0&rating=G&lang=en";
-
-	console.log(gifName);
-
-	$.ajax({url: apiLink, method: 'GET'}).done(function(response) {
-
-        console.log(response);
-
-        for (var i = 0; i < response.data.length; i++) {
-
-            var gifAddDiv = $('<div class="gifAdd">');
-			var theRating = response.data[i].rating;
-			var rateAddParagraph = $("<p>").text("rating... " + theRating);
-
-            gifAddDiv.append(rateAddParagraph);
-                
-            var gifPic = $("<img class='gif'>").attr("src", response.data[i].images.fixed_height.url);
-
-            gifAddDiv.append(gifPic);
-
-            $("#gifDiv").append(gifAddDiv);
-
-            gifAddDiv.addClass("pics");
-        }   
-	});
-};
-
-function generateButtons () {
-	$('.buttonsDiv').empty();
-
-	for (var i = listOfGifs.length - 1; i >= 0; i--) {
-		var addNewButton = $('<button>');
-		addNewButton.addClass('gifAdd');
-		addNewButton.attr('data-name', listOfGifs[i]);
-		addNewButton.text(listOfGifs[i]);
-		$('buttonsDiv').append(addNewButton);
+	//Array of nintendo characters
+	var topics = ["Mario", "Link", "Samus", "Kirby", "Fox McCloud"];
+	var resultNum = 10;
+  
+	//Function to display gifs on page
+	function displayGif() {
+  
+	  $('#gifs').empty();
+  
+	  var searchChar = $(this).attr("data-name");
+	  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchChar + "&api_key=snkpgzzy24XfiY7U9yoYkU93dX0FMQJF";
+  
+  
+	  console.log(searchChar);
+	  console.log(searchChar);
+  
+	  //Ajax Call
+	  $.ajax({
+		url: queryURL,
+		method: "GET"
+	  }).then(function (response) {
+  
+		console.log(response);
+  
+		$("#gif-container").empty();
+  
+		for (var i = 0; i < resultNum; i++) {
+  
+		  console.log(response);
+  
+		  var newDiv = $("<div class='gif-box'>");
+  
+		  var newGif = $("<img>").attr("src", response.data[i].images.fixed_height_still.url);
+  
+		  newGif.attr("data-still", response.data[i].images.fixed_height_still.url);
+		  newGif.attr("data-animate", response.data[i].images.fixed_height.url);
+		  newGif.attr("data-state", "still");
+		  newGif.addClass("gif");
+  
+		  var rating = response.data[i].rating;
+  
+		  var gifRating = $("<p>").text("Rating: " + rating)
+  
+		  newDiv.append(newGif);
+		  newDiv.append(gifRating);
+		  
+  
+		  $("#gif-container").prepend(newDiv);
+  
+  
+		};
+  
+	  });
 	}
-}
-
-$('.submitGif').on('click', function() {
-	var addedGif = $('.addPicInput').val().trim();
-
-	listOfGifs.push(addedGif);
-
-	generateButtons();
-
-	return false;
-});
-
-$(document).on('click', 'gifAdd', generatePics);
-
-generateButtons();
+  
+  
+	//Dynamic buttons created from items in array
+	function createButtons() {
+  
+	  $("#gif-buttons").empty();
+  
+	  for (i = 0; i < topics.length; i++) {
+  
+		var newGif = $("<button>");
+  
+		newGif.addClass("btn btn-danger charButton");
+  
+		newGif.attr("data-name", topics[i]);
+  
+		newGif.text(topics[i]);
+  
+		$("#gif-buttons").append(newGif)
+  
+	  }
+  
+	}
+  
+	//Take user input and create dynamic button
+	$("#add-gif").on("click", function () {
+  
+	  event.preventDefault();
+  
+	  var char = $("#char-input").val().trim();
+  
+	  topics.push(char);
+  
+	  createButtons();
+  
+	  $("form").trigger("reset");
+  
+	});
+  
+	//Function to animate gif on click
+	function animateGif() {
+  
+	  var state = $(this).attr("data-state");
+  
+	  if (state === "still") {
+		$(this).attr("src", $(this).attr("data-animate"));
+		$(this).attr("data-state", "animate");
+	  }
+	  else {
+		$(this).attr("src", $(this).attr("data-still"));
+		$(this).attr("data-state", "still");
+	  }
+  
+	};
+  
+	//Click listener for dynamic character buttons
+	$(document).on("click", ".charButton", displayGif);
+  
+	//Click listener to animate gifs
+	$(document).on("click", ".gif", animateGif);
+  
+	createButtons();
+  });
